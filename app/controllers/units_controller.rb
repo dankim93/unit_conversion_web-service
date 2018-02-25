a2f93eb29e046e37cf1276ad2b123225cdfb94fa
dankim93 <dan_kim93@hotmail.com>
2018-02-24 19:26:44 -0800

    Add extraction from params to numerators and denominators

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -9,8 +9,9 @@ class UnitsController < ApplicationController
       "d" => [86400, 's'],
       "degree" => [Math::PI/180, 'rad'],
       " " => [Math::PI/180, 'rad'],
-      "‘" => [Math::PI/10800], 'rad'],
+      "‘" => [Math::PI/10800, 'rad'],
       "second" => [Math::PI/648000, 'rad'],
+      "\"" => [Math::PI/648000, 'rad'],
       "hectare" => [10000, 'm^2'],
       "ha" => [10000, 'm^2'],
       "litre" => [0.001, 'm^3'],
@@ -18,7 +19,26 @@ class UnitsController < ApplicationController
       "tonne" => [10^3, 'kg'],
       "t" => [10^3, 'kg']
     }
+    answer = {
+      "unit_name" => "",
+      "multiplication_factor" => 1
+    }
+    numerator = params[:units].split("/").first.scan(/\w+/) +
+    params[:units].split("/").first.scan(/[\']/) +
+    params[:units].split("/").first.scan(/[\°]/) +
+    params[:units].split("/").first.scan(/[\"]/)
+
+    denominator = params[:units].split("/").last.scan(/\w+/) +
+    params[:units].split("/").last.scan(/[\']/) +
+    params[:units].split("/").last.scan(/[\°]/) +
+    params[:units].split("/").last.scan(/[\"]/)
+
+    # params[:units].split("/")
+    # params[:units].each_char do |c|
+
     # @test = params[:units]
-    # @test = table[" "]
+    # @test = table["\""]
+    @test = "ha*'"
+
   end
 end
